87a782623f97bc58dffca077234758db6425ee4e
kennedy <kimaniken2001@gmail.com>
2023-05-22 08:35:39 -0400

    completed with anagram detector lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w) }
+    end
+  
+    private
+  
+    def anagram?(word)
+      sorted_word(word) == sorted_word(@word) && word.downcase != @word.downcase
+    end
+  
+    def sorted_word(word)
+      word.downcase.chars.sort.join
+    end
+  end
+  
